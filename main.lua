9691039724268c96320a81d8fe9861e1dfb1fdb2
Tim Paduraru - Windows Desktop <timopaduraru@gmail.com>
2017-03-14 20:09:26 -0700

    still trying to fix those bad reverts

--- main.lua
+++ main.lua
@@ -26,20 +26,21 @@ local yCenter = (yMin + yMax) / 2
 local score = {}
 score.hits = 0
 score.misses = 0
-score.percent = math.floor(score.hits / (score.hits + score.misses))
-score.text = "Hits: " .. score.hits .. "Misses: " .. score.misses .. "Percent Hit: " .. score.percent
 
 -- image objects
 local bg
-local canon
+local canon 
 
 -- groups
 local bullets = display.newGroup()
 local targets = display.newGroup()
+
+-- Definitions for target images
 targets.russian = { "russian-hat.png", 500 / 5.5, 500 / 5.5, 4, -13, 3 }
 targets.german = { "german-hat.png", 2400 / 26, 1478 / 26, 4, -40, 5 }
 targets.runescape = {"rs-hat.png", 400 / 9, 400 / 9, 4, -45, 5}
 targets.types = { "russian", "german", "runescape" }
+
                   
 -- ███████╗██╗   ██╗███╗   ██╗ ██████╗████████╗██╗ ██████╗ ███╗   ██╗███████╗
 -- ██╔════╝██║   ██║████╗  ██║██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║██╔════╝
@@ -51,19 +52,21 @@ targets.types = { "russian", "german", "runescape" }
 -- refreshes game score depending on value passed into it.
 score.update = function(value)
 	if value == 1 then
-
+		score.hits = score.hits + 1
 	elseif value == -1 then
 		score.misses = score.misses + 1
 	end
 
+	-- updates score text
 	score.percent = math.floor(score.hits / (score.hits + score.misses))
-	score.text = "Hits: " .. score.hits .. "Misses: " .. score.misses .. "Percent Hit: " .. score.percent
+	score.text.text = "Hits: " .. score.hits .. ", Misses: " .. score.misses .. ", Percent Hit: " .. math.floor( 100 * score.hits / (score.hits + score.misses)) .. "%"
 end
 
 -- utility function to help add objects in one line
 function addImage( file, width, height, x, y, r, group )
 	local img
 
+	--checks if the user wants to add a group or not.
 	if group ~= nil then
 		img = display.newImageRect( group, file, width, height )
 	else
@@ -77,10 +80,18 @@ end
 
 -- creates all image objects
 function drawImages( ... )
+	-- background image
 	bg = addImage( "bg.jpg", 1920, 1080, xCenter, yCenter, 0, nil)
 	bg:toBack( )
+
+	-- canon
 	canon = addImage("elefun.png", 70, 145, xCenter, yMax, 0, nil)
 	canon.anchorY = 0.75
+
+	-- score
+	score.text = display.newText( "Hits: " .. score.hits .. ", Misses: " .. score.misses .. ", Percent Hit: " .. "100%",
+			xCenter, yMin + 30, native.systemFontBold, 15 )
+	score.text:toFront( )
 end
 
 -- whenever the bullet goes off the screen
@@ -88,6 +99,7 @@ function removeObj( obj )
 	obj:removeSelf( ) -- deletes image
 end
 
+-- once the target transition is done, it deletes the object
 function targetDone( t )
 	t:removeSelf( )
 	score.update(-1) -- adds one miss to sco
@@ -117,36 +129,57 @@ function fire( event )
 	canon:toFront()
 end
 
-function positionTarget( t, type )
-	-- positions target off screen randomly
+-- positions target off screen randomly
+function positionTarget( target, type )
 
-	if type == 1 then -- if russian
+	local t = targets[targets.types[type]]
+	print( targets.types[type] )
+	target.x = math.random( t.originMinX, t.originMaxX ) 
+	target.y = math.random( t.originMinY, t.originMaxY )
 
-		t.x = math.random( t[1].width + xMax, t[1].width + xMax + 10)
-		t.y = math.random( yMin, yMax - canon.height )
-
-		transition.to(t, {x = xMin - math.random(100, 400), rotation = math.random(360, 1000), 
-				time = math.random(5000, 10000), onComplete = targetDone})
-
-	elseif type == 2 then -- if german
-
-		t.x = math.random( xMin - t[1].width - 10, xMin - t[1].width )
-		t.y = math.random( yMin, yMax - canon.height )
-
-		transition.to(t, {x = xMax + math.random(100, 400), rotation = math.random(-1000, -300), 
-				time = math.random(5000, 10000), onComplete = targetDone})
-
-	elseif type == 3 then -- if runescape
-
-		t.x = math.random( xMin, xMax)
-		t.y = math.random( yMin - 100, yMax - 500 )
-
-		transition.to(t, {y = yMax + math.random(100, 400), rotation = math.random(360, 1000), 
-				time = math.random(2000, 4000), onComplete = targetDone})
-
-	end
+	transition.to(target, {
+			x = math.random(targets.destMinX, targets.destMaxX), 
+			y = math.random(targets.destMinY, targets.destMaxY),
+			alpha = t.destAlpha,
+			rotation = math.random(targets.rotationMin, targets.rotationMax), 
+			time = math.random(targets.timeMin, targets.timeMax), 
+			onComplete = targetDone
+			})
 end
 
+-- definitions for target transitions
+function initTargets()
+	-- general definitions
+	targets.timeMin = 5000
+	targets.timeMax = 8000
+	targets.rotationMin = 180
+	targets.rotationMax = 1800
+	targets.destMinX = xMin - 400 
+	targets.destMaxX = xMin - 100
+	targets.originMinX = 320 / 5 + xMax
+	targets.originMaxX = 320 / 5 + xMax + 50
+	targets.destMinY = yMin
+	targets.destMaxY = yMax - canon.height
+	targets.originMinY = yMin
+	targets.originMaxY = yMax - canon.height
+
+	-- russian definitions
+	targets.russian.destAlpha = 1 
+	targets.russian.destMaxX = xMin - 100
+	targets.russian.originMinX = 320 / 5 + xMax
+
+	-- german definitions
+	targets.german.destAlpha = 1
+	targets.german.destMaxX = xMax + 400
+	targets.german.originMinX = xMin - 320 / 5 - 50
+
+	-- runescape definitions
+	targets.runescape.destAlpha = 0 
+	targets.runescape.destMaxX = xMax + 400
+	targets.runescape.originMinX = xMin - 320 / 5 - 50
+end
+
+-- creates the new target with different hats
 function newTarget(  )
 	local t = display.newGroup() -- new display group for the target
 	local type = math.random(1,3)
@@ -161,47 +194,48 @@ function newTarget(  )
 	positionTarget(t, type)
 end
 
-function moveTargets(  )
-	for i = 1, targets.numChildren do
-		if targets[i] ~= nil then
-			targets[i].x = targets[i].x - 10
-			targets[i].rotation = targets[i].rotation + 1
-		end
-	end
-end
-
+-- once bulllet hits target
 function hit( t, b )
+	-- stops both of the transitions
 	transition.cancel(t)
 	transition.cancel(b)
 
+	-- shows an explosion that grows and fades out
 	e = addImage("explosion.png", 200 / 4, 211 / 4, t.x, t.y, 0, nil)
 	transition.to(e, {xScale = 2, yScale = 2, alpha = 0, onComplete = removeObj})
 
-	t:removeSelf( )
+	-- remove those objects
+	t:removeSelf( ) 
 	b:removeSelf( )
-
+ 
+	-- adds one hit
 	score.update(1)
 end
 
 -- checks for collisions
 function testForHits(  )
-	for i = 1, targets.numChildren do
-		for j = 1, bullets.numChildren do
+	for i = 1, targets.numChildren do -- for every target
+		for j = 1, bullets.numChildren do -- for every bullets
+
 			if math.abs(targets[i].x - bullets[j].x) <= (targets[i].width / 2)  + (bullets[j].width / 2)  and
 				math.abs(targets[i].y - bullets[j].y) <= (targets[i].width / 2)  + (bullets[j].height / 2) then
 				hit( targets[i], bullets[j] )
 				return true
 			end
+
 		end
 	end
 end
 
 -- -- runs whenever a new frame is drawn
 function enterFrame( event )
-	local n = math.random( 1, 1000 )
-	if n <= 30 then
+
+	-- 30 in 1000 chance to create a new target (3%)
+	if math.random( 1, 1000 ) <= 30 then
 		newTarget()
 	end
+
+	-- tests for bullet and target collisions
 	testForHits()
 end
 
@@ -219,9 +253,13 @@ function initGame()
 	-- draw display objects
 	drawImages()
 
+	-- inits the target variables
+	initTargets()
+
 	-- event listeners
 	Runtime:addEventListener( "touch", fire )
 	Runtime:addEventListener( "enterFrame", enterFrame)
 end
 
+-- starts the game
 initGame()
